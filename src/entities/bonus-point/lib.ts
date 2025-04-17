25e13706ab314e2f347d29e6d78039ebb3483deb
Jin Kwan Kim <kkole3897@gmail.com>
2025-04-18 04:42:50 +0900

    refactor: bonus point typescript 변경

--- src/entities/bonus-point/lib.ts
+++ src/entities/bonus-point/lib.ts
@@ -1,4 +1,4 @@
 import { BONUS_POINT_RATE } from "./constants";
 
-export const calcBonusPoints = (totalAmount) =>
+export const calcBonusPoints = (totalAmount: number) =>
   Math.floor(totalAmount * BONUS_POINT_RATE);
