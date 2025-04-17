8cb371e49ce993c7362832d03fb1c3b93bf99473
Jin Kwan Kim <kkole3897@gmail.com>
2025-04-18 03:53:37 +0900

    refactor: product entity typescript로 변경

--- src/entities/product/constants.ts
+++ src/entities/product/constants.ts
@@ -1,7 +1,7 @@
 export const WORDS = {
   outOfStock: "품절",
-};
+} as const;
 
 export const MESSAGES = {
   outOfStock: "재고가 부족합니다.",
-};
+} as const;
--- src/entities/product/lib.ts
+++ src/entities/product/lib.ts
@@ -1,4 +1,6 @@
-export const getInitialProducts = () => [
+import { type Product } from "./model";
+
+export const getInitialProducts = (): Product[] => [
   { id: "p1", name: "상품1", price: 10000, quantity: 50 },
   { id: "p2", name: "상품2", price: 20000, quantity: 30 },
   { id: "p3", name: "상품3", price: 30000, quantity: 20 },
@@ -6,14 +8,16 @@ export const getInitialProducts = () => [
   { id: "p5", name: "상품5", price: 25000, quantity: 10 },
 ];
 
-export const hasStock = (product) => product.quantity > 0;
+export const hasStock = (product: Product) => product.quantity > 0;
 
-export const isStockLow = (product) => product.quantity < 5;
+export const isStockLow = (product: Product) => product.quantity < 5;
 
-export const isProductQuantityMoreOrEqual = (product, quantity) =>
-  product.quantity >= quantity;
+export const isProductQuantityMoreOrEqual = (
+  product: Product,
+  quantity: number,
+) => product.quantity >= quantity;
 
-export const findProductById = (products, id) => {
+export const findProductById = (products: Product[], id: string) => {
   const found = products.find((product) => product.id === id);
 
   if (found) {
